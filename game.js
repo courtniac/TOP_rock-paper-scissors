6e91cfa85d7c0f34b63147499d96739c8139eaf0
Courtnie Chan <20526504+courtniac@users.noreply.github.com>
2023-12-04 23:32:58 -0800

    Add UI to rock paper scissors

--- game.js
+++ game.js
@@ -1,54 +1,63 @@
+let computerScore = 0;
+let playerScore = 0;
+let gameOver = false;
+
+const buttons = document.getElementById("buttons");
+for (const button of buttons.children) {
+    button.addEventListener("click", () => {
+        playRound(button.id);
+    });
+}
+
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
 
     return choices[Math.floor(Math.random() * 3)];
 }
 
-function getPlayerChoice() {
-    return prompt("Rock, paper, or scissors?");
-}
+function playRound(playerSelection) {
+    const computerSelection = getComputerChoice();
+    const resultDiv = document.getElementById("result");
 
-function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.length > 0 ? playerSelection.trim().toLowerCase() : "";
-
-    if (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors") {
-        return "Invalid play. Please try again.";
-    } else if (playerSelection === computerSelection) {
-        return "Draw! Try again.";
+    if (playerSelection === computerSelection) {
+        resultDiv.textContent = "Draw! Try again.";
     } else if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "paper" && computerSelection === "rock" || playerSelection === "scissors" && computerSelection === "paper") {
-        return `You won this round! (${playerSelection} beats ${computerSelection})`;
+        updateScore(true);
+
+        if (!gameOver) {
+            resultDiv.textContent = `You won this round! (${playerSelection} beats ${computerSelection})`;
+        }
     } else {
-        return `You lost this round. (${computerSelection} beats ${playerSelection})`;
-    }
-}
+        updateScore(false);
 
-function game() {
-    console.log("Let's play Rock, Paper, Scissors!");
-    let computerScore = 0;
-    let playerScore = 0;
-    let round = 1;
-
-    while (round<6) {
-        console.log(`- Round ${round} of 5 -\nComputer: ${computerScore}\nYou: ${playerScore}`);
-        let computerChoice = getComputerChoice();
-        let playerChoice = getPlayerChoice();
-        let result = playRound(playerChoice, computerChoice);
-        console.log(result);
-        
-        if (result.includes("won")) {
-            playerScore++;
-            round++;
-        } else if (result.includes("lost")) {
-            computerScore++;
-            round++;
+        if (!gameOver) {
+            resultDiv.textContent = `You lost this round. (${computerSelection} beats ${playerSelection})`;
         }
     }
 
-    if (computerScore > playerScore) {
-        console.log(`Sorry, you lost to the computer :(\n- Final score -\nComputer: ${computerScore}\nYou: ${playerScore}`);
-    } else {
-        console.log(`Congrats! You beat the computer!\n- Final score -\nComputer: ${computerScore}\nYou: ${playerScore}`);
+    if (gameOver) {
+        for (const button of buttons.children) {
+            button.disabled = true;
+        }
+        
+        resultDiv.textContent = playerScore === 5 ? "Congrats! You beat the computer! Refresh to try again." : "Sorry, you lost to the computer :( Refresh to try again.";
     }
 }
 
-game();
+function updateScore(playerWon) {
+    if (playerWon) {
+        playerScore++;
+    } else {
+        computerScore++;
+    }
+
+    if (computerScore === 5 || playerScore === 5) {
+        gameOver = true;
+    }
+
+    const computerScoreSection = document.getElementById("computerScore");
+    const playerScoreSection = document.getElementById("yourScore");
+
+    computerScoreSection.textContent = computerScore;
+    playerScoreSection.textContent = playerScore;
+}
